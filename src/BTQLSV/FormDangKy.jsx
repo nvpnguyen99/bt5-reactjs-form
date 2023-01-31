6234f98ebc6a6d3012798207138f5638592abad7
NguyenNVP <nvpnguyen99@gmail.com>
2023-01-31 17:42:09 +0700

    Get Form Data

--- src/BTQLSV/FormDangKy.jsx
+++ src/BTQLSV/FormDangKy.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 
 export default class FormDangKy extends Component {
+
+  state = {
+    values: {
+      maSV: "",
+      hoTen: "",
+      sdt: "",
+      email: ""
+    }
+  }
+
+  handleOnChange = (event) => {
+    let {value, name}= event.target;
+
+    let newValues = { ...this.state.values};
+    newValues[name] = value;
+
+    this.setState({
+      values: newValues
+    })
+  }
+
   render() {
     return (
       <div className="row">
@@ -10,22 +31,25 @@ export default class FormDangKy extends Component {
               <div className="row">
                   <div className="col">
                       <label htmlFor="">Mã SV</label>
-                      <input name='maSV' type="text" className="form-control" placeholder="Mã SV" />
+                      <input onChange={(event) => {
+                        this.handleOnChange(event)
+                      }} name='maSV' type="text" className="form-control" placeholder="Mã SV" />
                   </div>
                   <div className="col">
                       <label htmlFor="">Họ tên</label>
-                      <input type="hoTen" className="form-control" placeholder="Họ tên" />
+                      {/* rút gọn (tự điền event) */}
+                      <input onChange={this.handleOnChange} type="hoTen" className="form-control" placeholder="Họ tên" />
                   </div>
               </div>
 
               <div className="row">
                   <div className="col">
                       <label htmlFor="">Số điện thoại</label>
-                      <input name='sdt' type="text" className="form-control" placeholder="Số điện thoại" />
+                      <input onChange={this.handleOnChange} name='sdt' type="text" className="form-control" placeholder="Số điện thoại" />
                   </div>
                   <div className="col">
                       <label htmlFor="">Email</label>
-                      <input type="email" className="form-control" placeholder="Email" />
+                      <input onChange={this.handleOnChange} type="email" className="form-control" placeholder="Email" />
                   </div>
               </div>
 
